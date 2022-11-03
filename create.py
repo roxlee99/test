4afdd561faadc2e846233e394209f8413919543c
roxlee99 <97080067+roxlee99@users.noreply.github.com>
2022-11-03 09:53:39 +0800

    Add files via upload

---
+++ create.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#coding: utf-8
+#-----------------------------
+# 宝塔Linux面板网站备份工具
+#-----------------------------
+
+import sys,os
+os.chdir('/www/server/panel')
+sys.path.append("class/")
+if sys.version_info[0] == 2: 
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+import public,db,time
+import panelSite
+
+
+
+if __name__ == "__main__":
+    sites = panelSite.panelSite()
+    get = public.dict_obj()
+    
+    site = sys.argv[1]
+    
+    
+    get.create_type = 'txt'
+    get.websites_content = '{"' + site + ',www.' + site + ',m.' + site + '|1|0|0|0":""}'
+    
+    print (get.websites_content)
+    #get.websites_content = sys.argv[1]
+    # uncuniversity.com
+    #print(sys.argv[1])
+    #get.websites_content ='{"88581988.com,www.88581988.com,m.88581988.com|1|0|0|0":"","crackpotcomics.com,www.crackpotcomics.com,m.crackpotcomics.com|1|0|0|0":""}'
+    #print(type(get.websites_content))
+    s = sites.create_website_multiple(get)
+    print(s)
