cb2fbbb8a0c7a9cca3448f882aa7000b84e2a726
roxlee99 <97080067+roxlee99@users.noreply.github.com>
2022-11-17 16:34:03 +0800

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,23 @@
+
+import sys,os
+panelPath = '/www/server/panel/'
+os.chdir(panelPath)
+sys.path.insert(0,panelPath + "class/")
+import public,db,time
+import panelSite
+
+
+
+if __name__ == "__main__":
+    
+    sql = db.Sql()
+    
+    
+    site = panelSite.panelSite()
+    get = public.dict_obj()
+    site_name = sys.argv[1]
+    get.id = sql.table('sites').where('name=?', (site_name,)).getField('id')
+    get.path = '1'
+    get.webname = site_name
+    s = site.DeleteSite(get)
+    print(s)
