669c6bd91c5d17d88bce83a64577edcb3089dbd3
roxlee99 <97080067+roxlee99@users.noreply.github.com>
2022-11-28 10:23:20 +0800

    Create list_backup3.py

---
+++ list_backup3.py
@@ -0,0 +1,26 @@
+import sys, os, json
+if sys.version_info[0] == 2:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+os.chdir('/www/server/panel')
+sys.path.append("class/")
+import public, db
+
+sql = db.Sql()
+sites = public.M('sites').field('name,id').select()
+#print(sites)
+
+backups = sql.table('backup').select()
+
+# for backup in backups:
+#     print(backup)
+            
+for site in sites:
+    backups = sql.table('backup').where('pid=?', (site['id'])).field(
+            'id,name,filename').select()
+    for backup in backups:
+        print(site['name'],'\t',backup['name'])
+   
+    
+    
+
